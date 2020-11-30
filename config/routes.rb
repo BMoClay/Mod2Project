e59889ad3493724a5a41939f5b1678cb5f34fc5e
BMoClay <benmorgancleveland@gmail.com>
2020-11-30 11:52:58 -0500

    create users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :comments, only: [:new, :show, :create, :destroy]
+  resources :users, only: [:new, :show, :create, :destroy]
+  resources :posts, only: [:index, :new, :show, :create, :update, :destroy]
+
 end
